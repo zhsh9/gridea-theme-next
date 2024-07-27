e7ae29f9dc0682ad0c2bd4b6355c08106cd465aa
zhsh9 <zhsh@connect.hku.hk>
2024-07-27 18:34:09 +0800

    [+] Update some config

--- style-override.js
+++ style-override.js
@@ -520,8 +520,14 @@ const generateOverride = (params) => {
   }
 
   if (!params.shadow) {
-    result += `.box-shadow-wrapper {
-      box-shadow: none !important;
+    // result += `.box-shadow-wrapper {
+    //   box-shadow: none !important;
+    // }`
+    result += ``
+  } else {
+    result += `.box-shadow-wrapper , .page.bg-color {
+    border-radius: initial;
+    box-shadow: 0 2px 2px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0.06), 0 1px 5px 0 rgba(0,0,0,0.12), 0 -1px 0.5px 0 rgba(0,0,0,0.09) !important;
     }`
   }
 
